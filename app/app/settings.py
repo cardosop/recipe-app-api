07fc46facd57f7407c670071a6034db55a329781
Paulo Cardoso <ph_cardoso@hotmail.com>
2024-01-12 08:40:13 -0300

    user api

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -133,6 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
-REST_FRAMEWWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spetactular.openapi.AutoSchema'
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
 }
